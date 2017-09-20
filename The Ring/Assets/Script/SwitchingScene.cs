d4dd3945dd82b388eca82bf34ea1bb7f3c209faf
TangTai <phamf574@gmail.com>
2017-09-20 16:51:41 +0700

    Tạo vài sườn cơ bản của các Scene
    Chỉ tạo Giao diện cơ bản, sườn, kiểu sao này có hình để dô, chưa code. Tạo một cái effect di chuyển m dô sửa thì sửa. Trong Main scene run lên vuốt con Joystick sẽ thấy.

---
+++ The Ring/Assets/Script/SwitchingScene.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class SwitchingScene : MonoBehaviour, IPointerClickHandler  {
+
+    public string SceneName;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
