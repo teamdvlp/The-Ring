8904da0c6766510f31dd0b9a7f18fa72a82d71d6
tiendvlp <tiendvlp@gmail.com>
2017-09-20 18:17:50 +0700

    bổ sung thêm cameraFollow

---
+++ The Ring/Assets/Script/Camera/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public float smoothing;
+	public Transform target;
+	public float YLimit;
+	// true: trái, false: phải
+	public bool direction;
+
+	private Vector3 offset;
+	void Start () {
+		this.offset = new Vector3 (this.transform.position.x - this.target.transform.position.x, this.transform.position.y - this.target.transform.position.y, -10);
+	}
+	void Update () {
+		follow ();
+	}
+
+	private void follow () {
+		if (this.target != null) {
+				this.gameObject.transform.position = Vector3.Lerp (new Vector3(this.transform.position.x, this.transform.position.y, -10f), this.target.transform.position + offset, this.smoothing * Time.deltaTime);
+			}
+		}
+}
