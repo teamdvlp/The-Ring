6b38c1d2ba4c960d8ceadcc5cc32570204634cc6
TangTai <phamf574@gmail.com>
2017-09-21 15:10:00 +0700

    Create Monster Movement Script and Update Position On Progress Bar

---
+++ The Ring/Assets/Script/Monster/MonsterMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterMovement : MonoBehaviour {
+
+    Transform monsterTransform;
+    public float moveSpeed;
+
+	// Use this for initialization
+	void Start () {
+        monsterTransform = gameObject.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+    }
+
+    void Move ()
+    {
+        monsterTransform.position = new Vector3(monsterTransform.position.x + moveSpeed * Time.deltaTime, monsterTransform.position.y, 0);
+    }
+}
---
+++ The Ring/Assets/TestAsset/TestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestScipt : MonoBehaviour {
+
+    public Image monsterPosition, playerPosition;
+    public Image street;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
