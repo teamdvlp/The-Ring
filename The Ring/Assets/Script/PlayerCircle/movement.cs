852ef71ce6f4235a5b024c5fc28527b236b7fbea
TangTai <phamf574@gmail.com>
2017-09-23 11:23:27 +0700

    Create prefab of 5 actor circle and The test Evil Image

--- The Ring/Assets/Script/PlayerCircle/movement.cs
+++ The Ring/Assets/Script/PlayerCircle/movement.cs
@@ -1,11 +1,11 @@
-<<<<<<< HEAD
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour {
 	public Trackpad trackpad;
-	void Start () {
+
+    void Start () {
 		
 	}
 	
@@ -14,23 +14,4 @@ public class Movement : MonoBehaviour {
 			trackpad.positionOffset = Vector2.zero;
 }
 }
-=======
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class Movement : MonoBehaviour {
-	public Trackpad trackpad;
-
-
-    
-	void Start () {
-		
-	}
-	
-	void Update () {
-			this.GetComponent<Rigidbody2D>().velocity = trackpad.positionOffset * 80;
-			trackpad.positionOffset = Vector2.zero;
-    }
-}
->>>>>>> efd8f170e65cad631cea448820fd7e05d72d02a3
