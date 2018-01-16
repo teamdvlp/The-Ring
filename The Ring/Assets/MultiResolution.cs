1f84254729b51bfeb54a62e7e7eea91ac215d266
859taisoma <phamf574@gmail.com>
2018-01-16 22:40:34 +0700

    Multi resol

---
+++ The Ring/Assets/MultiResolution.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiResolution : MonoBehaviour {
+    public Camera cam;
+    float aspect;
+
+    void Start()
+    {
+        aspect = cam.aspect;
+        Debug.Log("16/9 = " + (9f/16f));
+        if (aspect.Equals(9f/16f))
+        {
+            Debug.Log("HIHI");
+            transform.localScale = new Vector3(1,1,1);
+        } else if (cam.aspect == 9f / 18.5f)
+        {
+            transform.localScale = new Vector3(0.8558578f, 0.8558578f, 0.8558578f);
+        } 
+    }
+
+}
--- The Ring/Assets/Script/GamePlay/StartGame/StartGame.cs
+++ The Ring/Assets/Script/GamePlay/StartGame/StartGame.cs
@@ -7,7 +7,7 @@ using UnityEngine.EventSystems;
 public class StartGame : MonoBehaviour{
 
     public AudioSource openingDoorSound, unlockDoorSound;
-    public GameObject settingButton, shoppingButton, propertiesButton, exitButton;
+    public GameObject settingButton, shoppingButton, propertiesButton;
     public GameObject nameGame;
     public Animator leftPreventer, rightPreventer;
     public MonsterMovement monsterMovement;
@@ -16,11 +16,10 @@ public class StartGame : MonoBehaviour{
     public void Starts ()
     {
         openingDoorSound = GetComponent<AudioSource>();
-        settingButton.transform.Translate(Vector3.left * speed);
-        shoppingButton.transform.Translate(Vector3.left * speed);
-        exitButton.transform.Translate(Vector3.right * speed);
-        nameGame.transform.Translate(Vector3.up * speed);
-        propertiesButton.transform.Translate(Vector3.down * speed);
+        settingButton.SetActive(false);
+        shoppingButton.SetActive(false);
+        nameGame.SetActive(false);
+        propertiesButton.SetActive(false);
         leftPreventer.enabled = true;
         rightPreventer.enabled = true;
         unlockDoorSound.Play();
