ccdb47fed8c660e2949f8616283963d702fa009b
tiendvlp <tiendvlp@gmail.com>
2018-01-18 21:00:33 +0700

    đụ má m thằng Tiến ngu vãi

--- The Ring/Assets/MultiResolution.cs
+++ The Ring/Assets/MultiResolution.cs
@@ -7,36 +7,15 @@ public class MultiResolution : MonoBehaviour {
    	private float screenW;
 	private float screenH;
 	public float aspect;
-    public float Rate = -1;
-    private List<float> listRate;
 	void Start () {
 		screenH = Screen.height;
 		screenW = Screen.width;
 		aspect = screenH/screenW;
-		Rate = findRateScreen(aspect);
         processMultipleResolution ();
 	}
 	private void processMultipleResolution()
 	{
-        if (Rate == -1) {
-            return;
-        }
-        // chênh lệch quá nhiều thì không làm gì hết vì nếu scale thì nó sẽ bị méo nhiều
-        if (Math.Abs(Rate - aspect) > 0.15) {
-            return;
-        }
-        Camera.main.aspect = 1/Rate;
-        Camera.main.orthographicSize = (BiggestSprite.bounds.size.x*Rate)/2f;
+        Camera.main.aspect = 1/aspect;
+        Camera.main.orthographicSize = (BiggestSprite.bounds.size.x*aspect)/2f;
 	}
-
-    private float findRateScreen (float aspect) {
-        listRate = new List<float>() {16f/9f, 16/10f, 18.5f/9f, 18f/9f, 4f/3f, 5f/4f};
-        float Rate = listRate[0];
-        for (int i = 0; i < listRate.Count-1; i++) {
-            if ((Mathf.Abs(aspect - Rate)) > (Mathf.Abs(aspect - listRate[i]))) {
-                Rate = listRate[i];
-            }
-        }
-        return Rate;
-    }
 }
