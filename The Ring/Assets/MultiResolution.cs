92e834bdfe66a055b1134d77158e6b7d09fe778b
tiendvlp <tiendvlp@gmail.com>
2018-01-19 20:22:40 +0700

    sửa phần multi lại một tí

--- The Ring/Assets/MultiResolution.cs
+++ The Ring/Assets/MultiResolution.cs
@@ -4,18 +4,8 @@ using UnityEngine;
 using System;
 public class MultiResolution : MonoBehaviour {
     public SpriteRenderer BiggestSprite;
-   	private float screenW;
-	private float screenH;
-	public float aspect;
+    public Canvas mCanvas;
 	void Start () {
-		screenH = Screen.height;
-		screenW = Screen.width;
-		aspect = screenH/screenW;
-        processMultipleResolution ();
-	}
-	private void processMultipleResolution()
-	{
-        Camera.main.aspect = 1/aspect;
-        Camera.main.orthographicSize = (BiggestSprite.bounds.size.x*aspect)/2f;
+	        Camera.main.orthographicSize = (BiggestSprite.bounds.size.x/Camera.main.aspect)/2f;
 	}
 }
